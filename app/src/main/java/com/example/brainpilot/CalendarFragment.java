83b6cfaba671b0936dff93040f439d92fbd7ff64
abryckaert <bryckaert.anthony@gmail.com>
2023-05-17 15:51:32 +0200

    correcting error, adding separte layout for the calendar day view

--- app/src/main/java/com/example/brainpilot/CalendarFragment.java
+++ app/src/main/java/com/example/brainpilot/CalendarFragment.java
@@ -2,40 +2,21 @@ package com.example.brainpilot;
 
 import android.os.Bundle;
 import androidx.fragment.app.Fragment;
+
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CalendarView;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link CalendarFragment#newInstance} factory method to
- * create an instance of this fragment.
- *
- */
+
 public class CalendarFragment extends Fragment {
+    CalendarView calendarView;
 
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment CalendarFragment.
-     */
-    // TODO: Rename and change types and number of parameters
     public static CalendarFragment newInstance(String param1, String param2) {
         CalendarFragment fragment = new CalendarFragment();
         Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
         return fragment;
     }
 
@@ -46,16 +27,21 @@ public class CalendarFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_calendar, container, false);
+
+        View view = inflater.inflate(R.layout.fragment_calendar, container, false);
+        calendarView = (CalendarView) view.findViewById(R.id.calendarView);
+        calendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+            @Override
+            public void onSelectedDayChange(CalendarView view, int year, int month, int dayOfMonth) {
+                String date =  year + "/" + month + "/" + dayOfMonth;
+                Log.wtf("wtf","onSelectedDayChange" + date);
+            }
+        });
+        return view;
     }
 }
--- app/src/main/java/com/example/brainpilot/KanbanFragment.java
+++ app/src/main/java/com/example/brainpilot/KanbanFragment.java
@@ -13,10 +13,8 @@ import android.widget.CalendarView;
 public class KanbanFragment extends Fragment {
 
 
-    // TODO: Rename and change types of parameters
 
     public KanbanFragment() {
-        // Required empty public constructor
     }
 
     public static KanbanFragment newInstance() {
@@ -28,29 +26,13 @@ public class KanbanFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        CalendarView calenderView = findViewById(R.id.calendarView);
-        calenderView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
-            @Override
-            public void onSelectedDayChange(CalendarView view, int year, int month,
-                                            int dayOfMonth) {
-                String curDate = String.valueOf(dayOfMonth);
-                String Year = String.valueOf(year);
-                String Month = String.valueOf(month);
-
-                Log.e("date", Year + "/" + Month + "/" + curDate);
-            }
-        });
     }
+
+
         public void getDate() {
 
         }
 
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_kanban, container, false);
-    }
 
 
 }
