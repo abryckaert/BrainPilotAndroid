ccae9236efc616f1cff08a259a8a81b7628c35be
abryckaert <bryckaert.anthony@gmail.com>
2023-06-01 08:24:14 +0200

    Correcting event bugs

--- app/src/main/java/com/example/brainpilot/EventAddFragment.java
+++ app/src/main/java/com/example/brainpilot/EventAddFragment.java
@@ -1,5 +1,6 @@
 package com.example.brainpilot;
 
+import android.app.TimePickerDialog;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -14,22 +15,35 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.TimePicker;
+
+import org.w3c.dom.Text;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.TimeZone;
 
 public class EventAddFragment extends Fragment {
 
     Button buttonBack;
     Button buttonSave;
+    Button buttonSelectTime;
     Context thiscontext;
     EditText eventName;
     TextView dateLabel;
+    TextView timeLabel;
 
     int year;
     int month;
     int day;
+    String hour;
+    String min;
     public EventAddFragment(int year, int month, int dayOfMonth) {
         this.year = year;
         this.month = month;
         this.day = dayOfMonth;
+        this.hour = "";
+        this.min = "";
         Log.wtf("wtf","année " + year + "mois " + month + "jour " + day);
     }
 
@@ -52,7 +66,7 @@ public class EventAddFragment extends Fragment {
         thiscontext = container.getContext();
 
         initObjects(inflater, container, viewEvent);
-        dateLabel.setText(day+"/"+month+"/"+year);
+        dateLabel.setText("Date: "+day+"/"+month+"/"+year);
 
         buttonBack.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -60,13 +74,40 @@ public class EventAddFragment extends Fragment {
                 moveToEventCalendarPage(viewEvent);
             }
         });
-
         buttonSave.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 MyDatabaseHelper db = new MyDatabaseHelper(thiscontext);
                 String name = eventName.getText().toString();
-                db.addEvent(day, month, year, name);
+                db.addEvent(day, month, year, hour,min,name);
+            }
+        });
+
+        buttonSelectTime.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.wtf("wtf","pressed");
+                Calendar calendar = Calendar.getInstance();
+                int hours = calendar.get(Calendar.HOUR_OF_DAY);
+                int minu = calendar.get(Calendar.MINUTE);
+
+                TimePickerDialog timePickerDialog = new TimePickerDialog(thiscontext, androidx.appcompat.R.style.Theme_AppCompat_Dialog, new TimePickerDialog.OnTimeSetListener() {
+                    @Override
+                    public void onTimeSet(TimePicker timePicker, int hourOfTheDay, int minu) {
+                        Log.wtf("wtf","open hour picker");
+                        Calendar c = Calendar.getInstance();
+                        c.set(Calendar.HOUR_OF_DAY, hourOfTheDay);
+                        c.set(Calendar.MINUTE,minu);
+                        c.setTimeZone(TimeZone.getDefault());
+                        SimpleDateFormat formatHour = new SimpleDateFormat("k");
+                        SimpleDateFormat formatMin = new SimpleDateFormat("mm");
+                        hour = formatHour.format(c.getTime());
+                        min = formatMin.format(c.getTime());
+                        timeLabel.setText("Hour: "+ hour+ "H" +min);
+
+                    }
+                },hours,minu,false);
+                timePickerDialog.show();
             }
         });
 
@@ -76,7 +117,9 @@ public class EventAddFragment extends Fragment {
     private void initObjects(LayoutInflater inflater, ViewGroup container, View view){
         buttonBack = (Button) view.findViewById(R.id.backButton);
         buttonSave = (Button) view.findViewById(R.id.saveButton);
+        buttonSelectTime = (Button) view.findViewById(R.id.selectTime);
         dateLabel = (TextView) view.findViewById(R.id.eventDate);
+        timeLabel = (TextView) view.findViewById(R.id.eventTime);
         eventName = (EditText) view.findViewById(R.id.eventName);
     }
 
